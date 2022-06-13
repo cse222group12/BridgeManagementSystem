a2e80563934c1ecf72ffc68e8d6100474921e94d
baygut <berkaybaygut@gmail.com>
2022-06-13 22:44:03 +0300

    main system -> bms

--- src/bms_src/BMS.java
+++ src/bms_src/BMS.java
@@ -9,18 +9,16 @@ import koffman_src.HashtableChain;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.PriorityQueue;
-import java.util.Stack;
+import java.util.*;
 
 public class BMS extends DataBase implements IBMS {
     public static Person currentUser;
     protected static HashtableChain<String,Person> persons; //TODO it could be private, protected for testing
 
-    protected JGraph<City,Integer> cities;
+    protected static JGraph<City,Integer> cities;
 
     AVLTree<Vehicle> blackListVehicles;
+    private static TreeSet<Plate> blacklistPlates = new TreeSet<>();
 
 
 
@@ -78,5 +76,72 @@ public class BMS extends DataBase implements IBMS {
         cities.addNode(new City("Istanbul"));
         Menu.push(Menu.Welcome);
 
+    }
+
+
+    public static void addCity(City aCity){
+        cities.addNode(aCity);
+    }
+
+    public static Iterator<City> getCityIterator() {
+        return cities.getNodes().iterator();
+    }
+
+    public static int getNumCity() {
+        return cities.size();
+    }
+
+    public static City getCity(String cityName) {
+        return cities.getNode(new City(cityName));
+    }
+
+    /**
+     * A method that put the person in the hash table
+     * @param person that is want to add
+     * @return person
+     */
+    public static Person addPerson(Person person){
+        return persons.put(person.getUsername(),person);
+    }
+
+    /**
+     * A method that put the person in the hash table
+     * @param person that is want to remove
+     * @return person
+     */
+    public static Person removePerson(Person person){
+        return persons.remove(person.getUsername());
+    }
+
+    /**
+     * A method that add vehicle to blackList
+     * @param plate which wanted to add blackList
+     */
+    public static boolean addBlackList(Plate plate){
+        return blacklistPlates.add(plate);
+    }
+
+
+    /**
+     * A method that remove vehicle to blackList
+     * @param plate which wanted to remove blackList
+     * @return      true if this set contained the specified element
+     */
+    public static boolean removeBlackList(Plate plate){
+        return blacklistPlates.remove(plate);
+    }
+
+    public static Iterator<Plate> blacklistIterator() {
+        return blacklistPlates.iterator();
+    }
+
+    /**
+     * A method that get person from db
+     * @param id person`s id
+     * @return Person if exist
+     *              else return null
+     */
+    public static Person getPerson(String id){
+        return persons.get(id);
     }
 }
