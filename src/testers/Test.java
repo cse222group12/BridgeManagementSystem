d828b089dee5bac0fe7d5c82a783716c8ff3e797
adenzu <erencakar54@gmail.com>
2022-06-14 23:03:37 +0300

    Fixed test main func

--- src/testers/Test.java
+++ src/testers/Test.java
@@ -9,11 +9,7 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
-        Stopwatch.printDirectly(() -> {
-            int sum = 0;
-            for (int i = 0; i < 100; i++) sum += i;
-        }, 1000, "sum of first 100 numbers");
-//        BMS bms = new BMS();
+        BMS bms = new BMS();
 //        Date date = new Date(2032, 12,3,5,7);
 //        System.out.println(date);
 
